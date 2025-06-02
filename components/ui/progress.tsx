0b9534dcb653fde4877f3f732694034321863310
JATIN NEWAR <132223040+jatinxnewar@users.noreply.github.com>
2025-06-02 23:50:46 +0530

    Made progress like label

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils"
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { showLabel?: boolean }
+>(({ className, value, showLabel = true, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn("relative h-4 w-full overflow-hidden rounded-full bg-secondary", className)}
@@ -18,6 +18,11 @@ const Progress = React.forwardRef<
       className="h-full w-full flex-1 bg-primary transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
+    {showLabel && (
+      <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 text-xs font-semibold text-white drop-shadow">
+        {Math.round(value || 0)}%
+      </span>
+    )}
   </ProgressPrimitive.Root>
 ))
 Progress.displayName = ProgressPrimitive.Root.displayName
