aea346c5c033a0bc65ba29bef6de28def5afb204
JATIN NEWAR <132223040+jatinxnewar@users.noreply.github.com>
2025-06-11 15:14:53 +0530

    Fixed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { ThemeShortcut } from "@/components/theme-shortcut" // Only import the component
-import { useEffect } from "react"
-import { useTheme } from "next-themes"
+import { ThemeShortcut } from "@/components/theme-shortcut"
 
 const inter = Inter({ subsets: ["latin"] })
 
